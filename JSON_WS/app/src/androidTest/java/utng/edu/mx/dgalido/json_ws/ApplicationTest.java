50cd342d30b5b456f0d6c20e04c21cdaf1595eb0
dianaGaliAre <lauragal26@outlook.com>
2016-08-26 12:50:31 +1200

    Consumir Web Services con JSON

---
+++ JSON_WS/app/src/androidTest/java/utng/edu/mx/dgalido/json_ws/ApplicationTest.java
@@ -0,0 +1,13 @@
+package utng.edu.mx.dgalido.json_ws;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
